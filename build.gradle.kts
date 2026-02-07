7646778d8ad046929791b7b5c62043ed8fc1fcb7
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2026-02-07 13:00:12 -0600

    Add Kover plugin to Gradle build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ subprojects {
 	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 	apply(plugin = "org.springframework.boot")
 	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlinx.kover")
 
 	repositories {
 		mavenCentral()
